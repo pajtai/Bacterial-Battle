f2deab49dd84f8b08e4785706566602142116ee2
Peter <peter@ubuntu.(none)>
2012-09-29 23:19:20 -0700

    adding node modules to ignore and creating package.json to manage them

--- application/scripts/coffee/main.coffee
+++ application/scripts/coffee/main.coffee
@@ -1,3 +1,3 @@
 $ ->
   paper = Raphael("medium", 500, 500)
-  paper.circle(10, 10, 50);
+  paper.circle(100, 100, 5);
--- application/scripts/javascript/main.js
+++ application/scripts/javascript/main.js
@@ -3,7 +3,7 @@
   $(function() {
     var paper;
     paper = Raphael("medium", 500, 500);
-    return paper.circle(10, 10, 50);
+    return paper.circle(100, 100, 5);
   });
 
 }).call(this);
