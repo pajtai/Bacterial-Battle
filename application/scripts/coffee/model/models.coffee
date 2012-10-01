4ee9811041dc3009708030466c11c0f0d8f7646a
Peter <peter@ubuntu.(none)>
2012-09-30 19:36:44 -0700

    setting up model

--- application/scripts/coffee/model/models.coffee
+++ application/scripts/coffee/model/models.coffee
@@ -1,13 +1,24 @@
+BacB = window.BacB
+Config = BacB.Config
+
 class BacteriaModel extends Backbone.Model
 
   initialize: (@population) ->
-    @addBacteria
     @bacteria = {}
-
+    i for i in [0..@population] ->
+      @addBacteria()
 
   addBacteria: ->
     c = Config
     x = _.random(0 + c.BacteriumRadius, c.BoardWidth - c.BacteriumRadius);
     y = _.random(0 + c.BacteriumRadius, c.BoardHeight - c.BacteriumRadius);
+
+class BacteriumModel extends Backbone.Model
+
+  initialize: (@buid, @x, @y, @radius) ->
+
+class BacteriumCollection extends Backbone.Collection
+
+  model: BacteriumModel
 
 window.BacB.BacteriaModal = BacteriaModel
--- application/scripts/javascript/model/models.js
+++ application/scripts/javascript/model/models.js
@@ -1,5 +1,5 @@
 (function () {
-  var BacteriaModel, __hasProp = {}.hasOwnProperty,
+  var BacB, BacteriaModel, BacteriumCollection, BacteriumModel, Config, __hasProp = {}.hasOwnProperty,
       __extends = function (child, parent) {
       for (var key in parent) {
         if (__hasProp.call(parent, key)) child[key] = parent[key];
@@ -13,6 +13,10 @@
       return child;
       };
 
+  BacB = window.BacB;
+
+  Config = BacB.Config;
+
   BacteriaModel = (function (_super) {
 
     __extends(BacteriaModel, _super);
@@ -22,9 +26,24 @@
     }
 
     BacteriaModel.prototype.initialize = function (population) {
+      var i, _i, _j, _len, _ref, _ref1, _results, _results1;
       this.population = population;
-      this.addBacteria;
-      return this.bacteria = {};
+      this.bacteria = {};
+      _ref1 = (function () {
+        _results1 = [];
+        for (var _j = 0, _ref = this.population; 0 <= _ref ? _j <= _ref : _j >= _ref; 0 <= _ref ? _j++ : _j--) {
+          _results1.push(_j);
+        }
+        return _results1;
+      }).apply(this)(function () {
+        return this.addBacteria();
+      });
+      _results = [];
+      for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+        i = _ref1[_i];
+        _results.push(i);
+      }
+      return _results;
     };
 
     BacteriaModel.prototype.addBacteria = function () {
@@ -37,6 +56,39 @@
     return BacteriaModel;
 
   })(Backbone.Model);
+
+  BacteriumModel = (function (_super) {
+
+    __extends(BacteriumModel, _super);
+
+    function BacteriumModel() {
+      return BacteriumModel.__super__.constructor.apply(this, arguments);
+    }
+
+    BacteriumModel.prototype.initialize = function (buid, x, y, radius) {
+      this.buid = buid;
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+    };
+
+    return BacteriumModel;
+
+  })(Backbone.Model);
+
+  BacteriumCollection = (function (_super) {
+
+    __extends(BacteriumCollection, _super);
+
+    function BacteriumCollection() {
+      return BacteriumCollection.__super__.constructor.apply(this, arguments);
+    }
+
+    BacteriumCollection.prototype.model = BacteriumModel;
+
+    return BacteriumCollection;
+
+  })(Backbone.Collection);
 
   window.BacB.BacteriaModal = BacteriaModel;
 
