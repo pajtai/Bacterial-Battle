e6d97221aee88c0cfe860a6299abd27f7291a44f
Peter <peter@ubuntu.(none)>
2012-09-30 22:13:19 -0700

    adding mediator

---
+++ application/scripts/javascript/mediator/mediator.js
@@ -0,0 +1,41 @@
+(function () {
+  var BacB, Mediator, __hasProp = {}.hasOwnProperty,
+      __extends = function (child, parent) {
+      for (var key in parent) {
+        if (__hasProp.call(parent, key)) child[key] = parent[key];
+      }
+      function ctor() {
+        this.constructor = child;
+      }
+      ctor.prototype = parent.prototype;
+      child.prototype = new ctor();
+      child.__super__ = parent.prototype;
+      return child;
+      };
+
+  BacB = window.BacB;
+
+  Mediator = (function (_super) {
+
+    __extends(Mediator, _super);
+
+    function Mediator() {
+      return Mediator.__super__.constructor.apply(this, arguments);
+    }
+
+    Mediator.prototype.initialize = function (medium, bacteria) {
+      this.medium = medium;
+      this.bacteria = bacteria;
+    };
+
+    Mediator.prototype.bacteriumModelAdded = function (bacterium) {
+      return this.medium.addBacterium(bacterium);
+    };
+
+    return Mediator;
+
+  })(Backbone.Model);
+
+  window.BacB.Mediator = Mediator;
+
+}).call(this);
